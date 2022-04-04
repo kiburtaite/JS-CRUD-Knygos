6ce771eac5865cc70a6bf34d313aaf0cd12373d4
kiburtaite <kiburtaite@gmail.com>
2022-04-05 02:14:46 +0300

    get data

---
+++ main.js
@@ -0,0 +1,16 @@
+fetch("data.json")
+.then(res => res.json())
+.then(data => showbooks(data));
+
+const showbooks = (book) => {
+    book.forEach(book => {
+        document.querySelector("#books").innerHTML +=
+        `<img src="${book.virselis}">
+        <p>Autorius: ${book.autorius}</p>
+        <p>Pavadinimas: ${book.pavadinimas}</p>
+        <p>Leidykla: ${book.leidykla}</p>
+        <p>Leidimo metai: ${book.leidimo_metai} m.</p>
+        <p>Puslapių skaičius: ${book.puslapiu_skaicius} psl.</p>
+        <p>Temos: ${book.temos} </p>`
+    })
+}
